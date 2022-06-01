2fabc7cf4da53c1d02b9988d28cdaa0d4ec7dd23
ek3waka <mansurov.eduard96@gmail.com>
2022-06-01 21:51:19 +0300

    progress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import Cog from './cog.png'
 import Menu from './menu.png'
+import './style.css'
 import {Project, DisplayProject, CreateAddButton} from './project-list'
 import {CreateCard, CreateProjectMenu, CreateTodoMenu} from './create-card'
+import {ToDo, DisplayToDo, CreateProjectContainer} from './project-card'
 
-const wrapper = document.querySelector('.wrapper')
+
+const body = document.querySelector('body')
+
+const wrapper = document.createElement('div')
+wrapper.classList.add('wrapper')
+
+body.append(wrapper)
+
 
 //create header
 const header = document.createElement('header')
@@ -46,30 +55,21 @@ main.append(projectList)
 let example = new Project('Example')
 projectList.append(new DisplayProject(example).showProjectDOM())
 
+
+example.addToDoToProject(1)
+example.addToDoToProject(2)
+example.addToDoToProject(3)
+
+console.log(example)
+
 let example2 = new Project('Example2')
 projectList.append(new DisplayProject(example2).showProjectDOM())
 
 let addProjectButton = new CreateAddButton()
-projectList.append(addProjectButton.createAddButton())
+projectList.append(addProjectButton.createAddButton('Project'))
 
 
 
-
-let addProject = new CreateCard('Project')
-main.append(addProject.createMenu())
-
-
-
-
-
-
-
-
-
-
-
-
-//create footer
 const footer = document.createElement('footer')
 const legalInfo = document.createElement('div')
 legalInfo.classList.add('legal-info')
@@ -80,3 +80,40 @@ copyRight.textContent = 'All rights reserved'
 legalInfo.append(siteName, copyRight)
 footer.appendChild(legalInfo)
 wrapper.append(main, footer)
+
+
+
+
+document.querySelector('.add-project-button').addEventListener('click', () => {
+    let addProject = new CreateCard('Project')
+    main.append(addProject.createMenu())
+    
+    
+})
+
+
+let todoexample = new ToDo('Do smth', '29.01.12', 'highest')
+let todoexample2 = new ToDo('todoexample2', '01.01.42', 'lowest')
+
+example.addToDoToProject(todoexample)
+example.addToDoToProject(todoexample2)
+
+
+
+
+let showtodoexample = new DisplayToDo().DOM(todoexample)
+let showtodoexample2 = new DisplayToDo().DOM(todoexample2)
+let showProject =  new CreateProjectContainer().createprojectContainer(example)
+
+showProject.append(showtodoexample, showtodoexample2)
+
+main.append(showProject)
+
+
+
+
+
+menuBtn.addEventListener('click', () => {
+    projectList.classList.toggle('hidden')
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ToDoList',
+      favicon: "./src/check-circle.png",
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
