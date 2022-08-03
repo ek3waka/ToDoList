b41af7449fde91e273bfa44ed273647c1cc36a78
ekewaka <mansurov.eduard96@gmail.com>
2022-08-03 23:20:00 +0300

    done reading methods from localstorage

--- src/DOM modules/add-todo-toDOM.js
+++ src/DOM modules/add-todo-toDOM.js
@@ -2,12 +2,13 @@ import { ToDo } from '../Elements modules/create-todo.js'
 import { closeModal } from '../Common functions module/modal-functions.js'
 import { normalizeDate } from '../Common functions module/data-functions'
 import { DisplayToDo } from './show-todo-DOM'
-
+import { Project } from '../Elements modules/create-project.js'
 
 function addToDoByName(modal, targetProject) {
     const addNewToDoButton = document.querySelector('#NewToDo')
-  
+    
     addNewToDoButton.addEventListener('click', () => {
+        console.log(targetProject)
         const todoContainer = document.querySelector('.todo-container')
         const ToDoTitle = document.querySelector('input[placeholder="ToDo Title"]')
         const ToDoDueDate = document.querySelector('.todo-date-input')
